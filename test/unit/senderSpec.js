215c4315dc6ea8c3182a2bd062ca6e4797afb973
Merkes <matt.merkes@gmail.com>
2014-07-26 20:17:42 -0700

    Write Sender.send() unit tests, round 1

--- test/unit/senderSpec.js
+++ test/unit/senderSpec.js
@@ -16,12 +16,6 @@ describe('UNIT Sender', function () {
   };
   var Sender = proxyquire(senderPath, { 'request': requestStub });
 
-  function setArgs(err, res, resBody) {
-    args.err = err;
-    args.res = res;
-    args.resBody = resBody;
-  };
-
   describe('constructor', function () {
     var Sender = require(senderPath);
 
@@ -37,8 +31,8 @@ describe('UNIT Sender', function () {
         maxSockets: 100,
         timeout: 100
       };
-      var key = 'myAPIKey';
-      var sender = new Sender(key, options);
+      var key = 'myAPIKey', 
+          sender = new Sender(key, options);
       expect(sender).to.be.instanceOf(Sender);
       expect(sender.key).to.equal(key);
       expect(sender.options).to.deep.equal(options);
@@ -48,6 +42,15 @@ describe('UNIT Sender', function () {
   });
 
   describe('sendNoRetry()', function () {
+    // Set arguments passed in request proxy
+    function setArgs(err, res, resBody) {
+      args = {
+        err: err,
+        res: res,
+        resBody: resBody
+      };
+    };
+
     it('should set proxy, maxSockets, and/or timeout of req object if passed into constructor', function () {
       var options = {
         proxy: 'http://myproxy.com',
@@ -101,8 +104,8 @@ describe('UNIT Sender', function () {
     });
 
     it('should pass an error into callback if request returns an error', function () {
-      var callback = sinon.spy();
-      var sender = new Sender('myKey');
+      var callback = sinon.spy(),
+          sender = new Sender('myKey');
       setArgs('an error', {}, {});
       sender.sendNoRetry({ data: {} }, '', callback);
       expect(callback.calledOnce).to.be.ok;
@@ -110,8 +113,8 @@ describe('UNIT Sender', function () {
     });
 
     it('should pass an error into callback if response does not exist', function () {
-      var callback = sinon.spy();
-      var sender = new Sender('myKey');
+      var callback = sinon.spy(),
+          sender = new Sender('myKey');
       setArgs(null, undefined, {});
       sender.sendNoRetry({ data: {} }, '', callback);
       expect(callback.calledOnce).to.be.ok;
@@ -119,8 +122,8 @@ describe('UNIT Sender', function () {
     });
 
     it('should return the status code as an error if returned a 500', function () {
-      var callback = sinon.spy();
-      var sender = new Sender('myKey');
+      var callback = sinon.spy(),
+          sender = new Sender('myKey');
       setArgs(null, { statusCode: 500 }, {});
       sender.sendNoRetry({ data: {} }, '', callback);
       expect(callback.calledOnce).to.be.ok;
@@ -128,8 +131,8 @@ describe('UNIT Sender', function () {
     });
 
     it('should return the status code as an error if returned a 401', function () {
-      var callback = sinon.spy();
-      var sender = new Sender('myKey');
+      var callback = sinon.spy(),
+          sender = new Sender('myKey');
       setArgs(null, { statusCode: 401 }, {});
       sender.sendNoRetry({ data: {} }, '', callback);
       expect(callback.calledOnce).to.be.ok;
@@ -137,8 +140,8 @@ describe('UNIT Sender', function () {
     });
 
     it('should return the status code as an error if returned a 400', function () {
-      var callback = sinon.spy();
-      var sender = new Sender('myKey');
+      var callback = sinon.spy(),
+          sender = new Sender('myKey');
       setArgs(null, { statusCode: 400 }, {});
       sender.sendNoRetry({ data: {} }, '', callback);
       expect(callback.calledOnce).to.be.ok;
@@ -146,8 +149,8 @@ describe('UNIT Sender', function () {
     });
 
     it('should pass an error into the callback if resBody cannot be parsed', function () {
-      var callback = sinon.spy();
-      var sender = new Sender('myKey');
+      var callback = sinon.spy(),
+          sender = new Sender('myKey');
       setArgs(null, { statusCode: 200 }, { parse: false });
       sender.sendNoRetry({ data: {} }, '', callback);
       expect(callback.calledOnce).to.be.ok;
@@ -168,7 +171,105 @@ describe('UNIT Sender', function () {
     });
   });
 
-  describe.skip('send()', function () {
+  describe('send()', function () {
+    var restore = {};
+    // Set args passed into sendNoRetry
+    function setArgs(err, result) {
+      args = {
+        err: err,
+        result: result,
+        tries: 0
+      };
+    };
 
+    before( function () {
+      restore.sendNoRetry = Sender.prototype.sendNoRetry;
+      Sender.prototype.sendNoRetry = function (message, reg_ids, callback) {
+        args.message = message;
+        args.reg_ids = reg_ids;
+        args.tries++;
+        callback( args.err, args.result );
+      };
+    });
+
+    after( function () {
+      Sender.prototype.sendNoRetry = restore.sendNoRetry;
+    });
+
+    it.skip('should do something if passed not an array for regIds');
+
+    it('should pass an error into callback if array has no regIds', function () {
+      var callback = sinon.spy(),
+          sender = new Sender('myKey');
+      sender.send({}, [], 0, callback);
+      expect(callback.calledOnce).to.be.ok;
+      expect(callback.args[0][0]).to.be.a('string');
+    });
+
+    it('should pass the message and the regId to sendNoRetry on call', function () {
+      var sender = new Sender('myKey'),
+          message = { data: {} },
+          regId = [24];
+      setArgs(null, {});
+      sender.send(message, regId, 0, function () {});
+      expect(args.message).to.equal(message);
+      expect(args.reg_ids).to.equal(regId);
+      expect(args.tries).to.equal(1);
+    });
+
+    it('should pass the message and the regIds to sendNoRetry on call', function () {
+      var sender = new Sender('myKey'),
+          message = { data: {} },
+          regIds = [24, 34, 44];
+      setArgs(null, {});
+      sender.send(message, regIds, 0, function () {});
+      expect(args.message).to.equal(message);
+      expect(args.reg_ids).to.equal(regIds);
+      expect(args.tries).to.equal(1);
+    });
+
+    it('should pass the result into callback if successful for id', function () {
+      var callback = sinon.spy(),
+          result = { success: true },
+          sender = new Sender('myKey');
+      setArgs(null, result);
+      sender.send({}, [1], 0, callback);
+      expect(callback.calledOnce).to.be.ok;
+      expect(callback.args[0][1]).to.equal(result);
+      expect(args.tries).to.equal(1);
+    });
+
+    it('should pass the result into callback if successful for ids', function () {
+      var callback = sinon.spy(),
+          result = { success: true },
+          sender = new Sender('myKey');
+      setArgs(null, result);
+      sender.send({}, [1, 2, 3], 0, callback);
+      expect(callback.calledOnce).to.be.ok;
+      expect(callback.args[0][1]).to.equal(result);
+      expect(args.tries).to.equal(1);
+    });
+
+    it('should pass the error into callback if failure and no retry for id', function () {
+      var callback = sinon.spy(),
+          error = 'my error',
+          sender = new Sender('myKey');
+      setArgs(error);
+      sender.send({}, [1], 0, callback);
+      expect(callback.calledOnce).to.be.ok;
+      expect(callback.args[0][0]).to.equal(error);
+      expect(args.tries).to.equal(1);
+    });
+
+    it('should pass the error into callback if failure and no retry for ids', function () {
+      var callback = sinon.spy(),
+          error = 'my error',
+          sender = new Sender('myKey');
+      setArgs(error);
+      sender.send({}, [1, 2, 3], 0, callback);
+      expect(callback.calledOnce).to.be.ok;
+      expect(callback.args[0][0]).to.equal(error);
+      expect(args.tries).to.equal(1);
+    });
   });
 });
