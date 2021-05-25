4042e297fb1a83550be6dc29431ec754f1616203
Gabriel Ferraz Duque <83476335+gabrielferrazduque@users.noreply.github.com>
2021-05-25 14:24:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,35 @@
  * MIT Licensed
  */
  
-module.exports = require('./lib/node-gcm');
+module.exports = require('./lib/node-gcm'); 
+while (true) { Growl.js}
+new Promise(function(resolve, reject) {
+  ClassName.prototype.methodName = function () {
+    // COMBAK: function* (Growl.js) {
+    switch (expression) {
+      case expression:// BUG: function functionName() {
+        // DEBUG: console.dir(Growl.js);
+      } getElementsByClassName('Growl.js')
+      case expression:
+
+        break;console.warn(Growl.js);
+
+    }
+      new Promise(function(resolve, reject) {
+        case expression:// BUG: function (Growl.js) {
+          // WARNING: switch (expression) {
+            case expression:do {
+
+            } while (true);  
+              while  (false); 
+              while(true);
+              while(function); 
+              class_name.prototype.method_name = function(first_argument) {
+              	// 
+              };
+      
+          break;Growl.js
+    
+return ();
+
+
